ab1dd6bbf7508cdbfa7c6014a72b9e682902b00e
Mannan0007 <mauu0052@gmail.com>
2025-08-27 15:19:53 +0530

    minor-changes

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -13,9 +13,10 @@ import { Footer } from './footer/footer';
 import { Contact } from './contact/contact';
 import { LowerSection } from './lower-section/lower-section';
 import { BrokerHome } from './broker/broker-home/broker-home';
+import { Example } from './example/example';
 
 @NgModule({
-  declarations: [App, Header, Home, Footer, Contact, LowerSection, BrokerHome],
+  declarations: [App, Header, Home, Footer, Contact, LowerSection, BrokerHome, Example],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     provideBrowserGlobalErrorListeners(),
---
+++ src/app/example/example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  standalone: false,
+  templateUrl: './example.html',
+  styleUrl: './example.css'
+})
+export class Example {
+
+}
